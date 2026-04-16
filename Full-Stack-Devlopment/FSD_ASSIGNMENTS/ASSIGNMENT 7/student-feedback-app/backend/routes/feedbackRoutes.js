4990b926a066e7890a1cecf6981d85b357e233e7
Pritesh Bagul <159455046+priteshbagul07@users.noreply.github.com>
2026-04-16 23:04:29 +0530

    Delete Full-Stack-Devlopment/FSD_ASSIGNMENTS/ASSIGNMENT 7 directory

--- Full-Stack-Devlopment/FSD_ASSIGNMENTS/ASSIGNMENT 7/student-feedback-app/backend/routes/feedbackRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getFeedbacks, createFeedback } = require('../controllers/feedbackController');
-
-router.get('/', getFeedbacks);
-router.post('/', createFeedback);
-
-module.exports = router;
